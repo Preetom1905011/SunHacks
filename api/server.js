c3ef71ae817173f60566ba5a3ea439e74877c853
architsing0 <5ingh4rch13@gmail.com>
2023-11-04 20:21:28 -0700

    Possible WebHook implementation

--- api/server.js
+++ api/server.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const passport = require('passport');
 const session = require('express-session');
-const { Octokit } = require('octokit')
+const { Octokit } = require('@octokit/rest')
+const { Webhooks } = require("@octokit/webhooks");
+
 
 const app = express();
 
@@ -74,6 +76,66 @@ app.get('/auth/github/callback',
     // Successful authentication, redirect home.
     res.redirect('/get-username');
   });
+
+async function handleUserFlow(req, res) {
+  const webhooks = new Webhooks({
+    secret: process.env.WEBHOOK_SECRET,
+  });
+  if (!req.isAuthenticated()) {
+    return res.status(403).send('Not authenticated');
+  }
+
+  const octokit = getOctokit(req);
+  const owner = req.user.username;
+
+  try {
+    // Create repository
+    const createRepoResponse = await octokit.rest.repos.createForAuthenticatedUser({
+      name: 'test-repo',
+    });
+    const repo = createRepoResponse.data.name;
+
+    // Create webhook
+    const createWebhookResponse = await octokit.rest.repos.createWebhook({
+      owner,
+      repo,
+      config: {
+        url: 'https://13.56.80.187:5000/webhook',
+        content_type: 'json',
+        secret: process.env.WEBHOOK_SECRET,
+      },
+      events: ['push', 'pull_request'],
+    });
+    const webhookId = createWebhookResponse.data.id;
+
+    // Respond to push and pull_request webhook events
+    webhooks.on("push", ({ id, name, payload }) => {
+      console.log(`Push event received for ${payload.repository.url} to ${payload.ref}`);
+      // Your code to handle push events
+    });
+
+    webhooks.on("pull_request", ({ id, name, payload }) => {
+      console.log(`Pull_request event received for ${payload.repository.url}`);
+      // Your code to handle pull_request events
+    });
+
+    // Delete repository (for simplicity, deleting after a delay of 5 minutes)
+    setTimeout(async () => {
+      await octokit.rest.repos.delete({
+        owner,
+        repo,
+      });
+      console.log('Repository deleted');
+    }, 300000);
+
+    res.send('Repository and webhook created, webhook is active, repository will be deleted after 5 minutes');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred');
+  }
+}
+
+app.get('/handle-user-flow', handleUserFlow);
 
 app.listen(5000, () => {
     console.log('App listening on http://localhost:5000');
