1ac4559c5fba332fb9294500f279f17412406c51
architsing0 <5ingh4rch13@gmail.com>
2023-11-04 17:00:34 -0700

    Initial implementation of Github oauth

--- api/server.js
+++ api/server.js
@@ -1,11 +1,37 @@
-const express = require("express")
-const app = express()
+require('dotenv').config();
 
+const express = require('express');
+const passport = require('passport');
 
-app.get("/", function(req, res) {
-    res.send("It's working!")
-  })
+const app = express();
 
-app.listen(3000, () => {
-  console.log("app listening on port 3000")
-})
+var GitHubStrategy = require('passport-github').Strategy;
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:5000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ githubId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get('/auth/github',
+  passport.authenticate('github', function() {
+    
+  }));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+app.listen(5000, () => {
+    console.log('App listening on http://localhost:5000');
+});
+
