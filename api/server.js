c3ec968f4222aa18a7fd44339fdef8be343738f6
architsing0 <5ingh4rch13@gmail.com>
2023-11-04 17:23:57 -0700

    Session Management

--- api/server.js
+++ api/server.js
@@ -2,27 +2,39 @@ require('dotenv').config();
 
 const express = require('express');
 const passport = require('passport');
+const session = require('express-session');
 
 const app = express();
 
 var GitHubStrategy = require('passport-github').Strategy;
 
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:5000/auth/github/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ githubId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
+  callbackURL: "http://127.0.0.1:5000/auth/github/callback"
+},
+(accessToken, refreshToken, profile, done) => {
+  return done(null, profile);
+}
 ));
 
+// Set up session management using the secret from the .env file
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
+
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/auth/github',
-  passport.authenticate('github', function() {
-    
-  }));
+  passport.authenticate('github'));
 
 app.get('/auth/github/callback', 
   passport.authenticate('github', { failureRedirect: '/login' }),
@@ -34,4 +46,3 @@ app.get('/auth/github/callback',
 app.listen(5000, () => {
     console.log('App listening on http://localhost:5000');
 });
-
